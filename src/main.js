ecf230f9a9f0ccaf548f45d241815f9a9791c0b3
Andre Almeida <andrecasa@gmail.com>
2025-06-27 20:08:54 -0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,46 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+
+import Aura from '@primeuix/themes/aura';
+import PrimeVue from 'primevue/config';
+import ConfirmationService from 'primevue/confirmationservice';
+import ToastService from 'primevue/toastservice';
+
+import { definePreset } from '@primeuix/themes';
+
+import '@/assets/styles.scss';
+
+const app = createApp(App);
+
+const MyPreset = definePreset(Aura, {
+        semantic: {
+        primary: {
+            50: '{indigo.50}',
+            100: '{indigo.100}',
+            200: '{indigo.200}',
+            300: '{indigo.300}',
+            400: '{indigo.400}',
+            500: '{indigo.500}',
+            600: '{indigo.600}',
+            700: '{indigo.700}',
+            800: '{indigo.800}',
+            900: '{indigo.900}',
+            950: '{indigo.950}'
+        }
+    }
+});
+
+app.use(router);
+app.use(PrimeVue, {
+    theme: {
+        preset: MyPreset,
+        options: {
+            darkModeSelector: '.app-dark'
+        }
+    }
+});
+app.use(ToastService);
+app.use(ConfirmationService);
+
+app.mount('#app');
